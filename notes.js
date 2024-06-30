437596e6896446c78b88a2823f16755b762984dd
Aman Sharma <amansharma21ash@gmail.com>
2024-07-01 00:17:47 +0530

    first version of our hotels

---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('notes page is now visible');
+var age = 24;
+
+    const addnumbers = (a,b)=>{
+        return a+b;
+    }
+    module.exports = {
+        age,
+        addnumbers
+    }
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express();
+const db = require('./db');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+
+
+app.get('/',function (req,res){
+    res.send('welcome to my hotel... how i can help you ? we have menu..')
+})
+
+
+
+
+
+// app.get('/dosa',(req,res)=>{
+//     res.send('sure sir, i would love to serve dosa')
+// })
+
+// app.get('/idli',(req,res)=>{
+//     var customized_idli = {
+//         name: 'rava idli',
+//         size: '10 cm diameter',
+//         is_sambhar: true,
+//         is_chutney: false
+//     }
+//     res.send(customized_idli)
+// })
+
+// app.post('/items',(req,res)=>{
+//     res.send('data is saved ');
+// })
+
+
+const personRoutes = require('./routes/personRoutes');
+app.use('/person',personRoutes);
+
+const menuRoutes = require('./routes/menuRoutes');
+app.use('/menu',menuRoutes);
+
+
+app.listen(3000,()=>{
+    console.log('listening....on port 3000');
+})
